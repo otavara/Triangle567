465010fda28c47e744ad1ed795c309f52e36aab9
Oscar Tavara <otavara@gmail.com>
2018-09-06 20:05:01 -0400

    fix

--- TestTriangle.py
+++ TestTriangle.py
@@ -10,7 +10,6 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 import unittest
 
 from Triangle import classifyTriangle
-from jinja2.lexer import integer_re
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
